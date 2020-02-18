2bd6b3cbb7ff53ecb5a4d07a940be3e1ffc59f81
CalebRose <ctrose17@gmail.com>
2020-02-17 19:39:08 -0600

    Modified algorithm

--- autopost.py
+++ autopost.py
@@ -22,6 +22,7 @@ home_team = {
     "r_td": 0,
     "fum": 0,
     "lost": 0,
+    "xp": False,
     "xpm": 0,
     "xpa": 0,
     "xp_percent": 0,
@@ -30,8 +31,8 @@ home_team = {
     "fg_percent": 0,
     "kr_td": 0,
     "punts": 0,
-    "p_yds": 0,
-    "p_avg": 0,
+    "punt_yds": 0,
+    "punt_avg": 0,
     "kickoff_yds": 0,
     "rtn": 0,
     "rtn_yds": 0,
@@ -47,46 +48,49 @@ home_team = {
 }
 away_team = {
     "team": "",
-    "score": "",
-    "passing": "",
-    "p_attempts": "",
-    "p_completions": "",
-    "p_TD": "",
-    "int": "",
-    "sck": "",
-    "rushing": "",
-    "r_att": "",
-    "r_yds_att": "",
-    "r_td": "",
-    "fum": "",
-    "lost": "",
-    "xpm": "",
-    "xpa": "",
-    "xp_percent": "",
-    "fgm": "",
-    "fga": "",
-    "fg_percent": "",
-    "kr_td": "",
-    "punts": "",
-    "p_yds": "",
-    "p_avg": "",
-    "rtn": "",
-    "rtn_yds": "",
-    "rtn_avg": "",
-    "pr_td": "",
-    "saf": "",
-    "pen": "",
-    "pen_yds": "",
-    "q1_score": "",
-    "q2_score": "",
-    "q3_score": "",
-    "q4_score": ""
+    "score": 0,
+    "passing": 0,
+    "p_attempts": 0,
+    "p_completions": 0,
+    "p_td": 0,
+    "int": 0,
+    "sck": 0,
+    "rushing": 0,
+    "r_att": 0,
+    "r_yds_att": 0,
+    "r_td": 0,
+    "fum": 0,
+    "lost": 0,
+    "xp": False,
+    "xpm": 0,
+    "xpa": 0,
+    "xp_percent": 0,
+    "fgm": 0,
+    "fga": 0,
+    "fg_percent": 0,
+    "kr_td": 0,
+    "punts": 0,
+    "punt_yds": 0,
+    "punt_avg": 0,
+    "kickoff_yds": 0,
+    "rtn": 0,
+    "rtn_yds": 0,
+    "rtn_avg": 0,
+    "pr_td": 0,
+    "saf": 0,
+    "pen": 0,
+    "pen_yds": 0,
+    "q1_score": 0,
+    "q2_score": 0,
+    "q3_score": 0,
+    "q4_score": 0
 }
 
 stadium = ""
 location = ""
 iterator = 0
 gaps = (7, 42, 43, 79, 80, 81, 82, 118, 119)
+# Tuples to hold the different offensive plays to run
 pass_plays = ('Slants', 'Flood', 'Scissors', 'Play Action',
               'Screen Pass', 'Max Protect', 'Streaks')
 rush_plays = ('Blast', 'Power', 'Draw', 'Sweep', 'Option')
@@ -96,185 +100,142 @@ rush_plays = ('Blast', 'Power', 'Draw', 'Sweep', 'Option')
 #     for row in csv_reader:
 #         print(row)
 
-with open("./NYC@PP.csv") as file:
+# Function to allocate points by quarter
+
+
+def score_allocator(team, iterator, points):
+    if(iterator < 42):
+        team['q1_score'] += points
+    elif(iterator < 79):
+        team['q2_score'] += points
+    elif(iterator < 118):
+        team['q3_score'] += points
+    else:
+        team['q4_score'] += points
+
+
+def play_allocation(offense, defense, row, iterator):
+    #
+    play = row[2]
+    pass_play = False
+    rush_play = False
+    # kickoff = row[4]
+    p_yds = int(row[8])
+    incomplete = row[9]
+    r_yds = int(row[10])
+    k_yards = int(row[11])
+    punt_yards = int(row[12])
+    ret_yrds = int(row[13])
+    sack = row[14]
+    fg = row[15]
+    td = row[16]
+    saf = row[17]
+    inter = row[18]
+    fum = row[19]
+    turnover = row[20]
+    minor_injury = row[21]
+    major_injury = row[22]
+    injury = row[23]
+    penalty = row[24]
+    penalty_type = row[25]
+    penalty_yards = int(row[26])
+    was_accepted = row[27]
+    # Pass Play
+    if(play in pass_plays):
+        pass_play = True
+        offense['passing'] += p_yds
+        if(incomplete == 'False'):
+            offense['p_completions'] += 1
+        if(sack == 'True'):
+            offense['sck'] += 1
+        else:
+            offense['p_attempts'] += 1
+        if(inter == 'True'):
+            offense['int'] += 1
+    elif(play in rush_plays):
+        print(play)
+        rush_play = True
+        offense['rushing'] += r_yds
+        if(was_accepted == 'False'):
+            offense['r_att'] += 1
+        offense['r_yds_att'] = float(offense['rushing']/offense['r_att'])
+        if(fum == 'True'):
+            offense['fum'] += 1
+            if(turnover == 'True'):
+                offense['lost'] += 1
+
+    elif(play == 'Kickoff' or play == 'Punt'):
+        if(play == 'Kickoff'):
+            offense['kickoff_yds'] += k_yards
+        elif(play == 'Punt'):
+            offense['punts'] += 1
+            offense['punt_yds'] += punt_yards
+            offense['punt_avg'] = float(offense['punt_yds'] /
+                                        offense['punts'])
+            # Calculate Away Team Return Yards
+            defense['rtn_yds'] += ret_yrds
+            defense['rtn'] += 1
+            defense['rtn_avg'] = float(defense['rtn_yds'] /
+                                       defense['rtn'])
+
+    # Touchdown Made
+    if(td == 'True'):
+        if(pass_play == True):
+            offense['p_td'] += 1
+        elif(rush_play == True):
+            offense['r_td'] += 1
+        score_allocator(offense, iterator, 6)
+        pass_play = False
+        rush_play = False
+        offense['xp'] = True
+    if(play == 'Field Goal'):
+        # If the FG is an extra point
+        if(offense['xp'] == True):
+            if(fg == 'True'):
+                score_allocator(offense, iterator, 1)
+                offense['xpm'] += 1
+            offense['xpa'] += 1
+            offense['xp_percent'] = float(offense['xpm'] / offense['xpa'])
+            offense['xp'] = False
+        # Else, it's just a field goal for 3
+        else:
+            if(fg == 'True'):
+                score_allocator(offense, iterator, 3)
+                offense['fgm'] += 1
+            offense['fga'] += 1
+            offense['fg_percent'] = float(offense['fgm'] / offense['fga'])
+
+    # Penalty Occurring
+    if(penalty == 'True'):
+        offense['pen'] += 1
+        if(was_accepted == 'True'):
+            offense['pen_yds'] += penalty_yards
+    if(saf == 'True'):
+        offense['saf'] += 1
+
+
+with open("") as file:
     csv_reader = reader(file)
     for row in csv_reader:
-        if(row[0] == 'Home Team:'):
-            home_team['team'] = row[1]
-            home_team['score'] = row[2]
-        elif(row[0] == 'Away Team:'):
-            away_team['team'] = row[1]
-            away_team['score'] = row[2]
+        if(iterator <= 6):
+            if(row[0] == 'Home Team:'):
+                home_team['team'] = row[1]
+                home_team['score'] = row[2]
+            elif(row[0] == 'Away Team:'):
+                away_team['team'] = row[1]
+                away_team['score'] = row[2]
+            elif(row[0] == 'Stadium:'):
+                stadium = row[1]
+                location = row[2]
         elif(iterator > 6 and iterator not in gaps):
-            play = row[2]
-            pass_play = False
-            rush_play = False
-            kickoff = row[4]
-            p_yds = row[8]
-            incomplete = row[9]
-            r_yds = row[10]
-            k_yards = row[11]
-            punt_yards = row[12]
-            ret_yrds = row[13]
-            sack = row[14]
-            fg = row[15]
-            td = row[16]
-            xp = False
-            saf = row[17]
-            inter = row[18]
-            fum = row[19]
-            turnover = row[20]
-            minor_injury = row[21]
-            major_injury = row[22]
-            injury = row[23]
-            penalty = row[24]
-            penalty_type = row[25]
-            penalty_yards = row[26]
-            was_accepted = row[27]
             # Home Team
             if(row[1] == home_team['team']):
-                # # Calculate stats # #
-                # Passing
-                if(play in pass_plays):
-                    pass_play = True
-                    home_team['passing'] += p_yds
-                    if(incomplete == 'FALSE'):
-                        home_team['p_completions'] += 1
-                    if(sack == 'TRUE'):
-                        home_team['sck'] += 1
-                    else:
-                        home_team['p_attempts'] += 1
-                    if(inter == 'TRUE'):
-                        home_team['int'] += 1
-
-                # Rushing
-                elif(play in rush_plays):
-                    rush_play = True
-                    home_team['rushing'] += r_yds
-                    home_team['r_att'] += 1
-                    home_team['r_yds_att'] = home_team['rushing'] / \
-                        home_team['r_att']
-                    if(fum == 'TRUE'):
-                        home_team['fum'] += 1
-                        if(turnover == 'TRUE'):
-                            home_team['lost'] += 1
-
-                elif(play == 'Kickoff' or play == 'Punt'):
-                    if(play == 'Kickoff'):
-                        home_team['kickoff_yds'] += k_yards
-                    elif(play == 'Punt'):
-                        home_team['punts'] += 1
-                        home_team['p_yds'] += punt_yards
-                        home_team['p_avg'] = home_team['p_yds'] / \
-                            home_team['punts']
-                    # Calculate Away Team Return Yards
-                    away_team['rtn_yds'] += ret_yrds
-                    away_team['rtn'] += 1
-                    away_team['rtn_avg'] = away_team['rtn_yds'] / \
-                        away_team['rtn']
-                # Touchdown Made
-                if(td == 'TRUE'):
-                    if(pass_play == True):
-                        home_team['p_td'] += 1
-                    elif(rush_play == True):
-                        home_team['r_td'] += 1
-                    if(iterator < 42):
-                        home_team['q1_score'] += 6
-                    elif(iterator < 79):
-                        home_team['q2_score'] += 6
-                    elif(iterator < 118):
-                        home_team['q3_score'] += 6
-                    else:
-                        home_team['q4_score'] += 6
-                    xp = True
-                if(play == 'Field Goal'):
-                    if(xp == True):
-                        if(fg == 'TRUE'):
-                            home_team['q1_score'] += 1
-                            home_team['xpm'] += 1
-                        home_team['xpa'] += 1
-                        xp = False
-                    else:
-                        if(fg == 'TRUE'):
-                            home_team['q1_score'] += 3
-                            home_team['fgm'] += 1
-                        home_team['fga'] += 1
-
-                if(penalty == 'TRUE'):
-                    home_team['pen'] += 1
-                    if(was_accepted == 'TRUE'):
-                        home_team['pen_yds'] += penalty_yards
+                # Offense = Home Team; Defense = Away Team
+                play_allocation(home_team, away_team, row, iterator)
+            # If the team is the AWAY team
             elif(row[1] == away_team['team']):
-                if(play in pass_plays):
-                    pass_play = True
-                    away_team['passing'] += p_yds
-                    if(incomplete == 'FALSE'):
-                        away_team['p_completions'] += 1
-                    if(sack == 'TRUE'):
-                        away_team['sck'] += 1
-                    else:
-                        away_team['p_attempts'] += 1
-                    if(inter == 'TRUE'):
-                        away_team['int'] += 1
-
-                # Rushing
-                elif(play in rush_plays):
-                    rush_play = True
-                    away_team['rushing'] += r_yds
-                    away_team['r_att'] += 1
-                    away_team['r_yds_att'] = away_team['rushing'] / \
-                        away_team['r_att']
-                    if(fum == 'TRUE'):
-                        away_team['fum'] += 1
-                        if(turnover == 'TRUE'):
-                            away_team['lost'] += 1
-
-                elif(play == 'Kickoff' or play == 'Punt'):
-                    if(play == 'Kickoff'):
-                        away_team['kickoff_yds'] += k_yards
-                    elif(play == 'Punt'):
-                        away_team['punts'] += 1
-                        away_team['p_yds'] += punt_yards
-                        away_team['p_avg'] = away_team['p_yds'] / \
-                            home_team['punts']
-                    # Calculate Away Team Return Yards
-                    home_team['rtn_yds'] += ret_yrds
-                    home_team['rtn'] += 1
-                    home_team['rtn_avg'] = home_team['rtn_yds'] / \
-                        home_team['rtn']
-                # Touchdown Made
-                if(td == 'TRUE'):
-                    if(pass_play == True):
-                        away_team['p_td'] += 1
-                    elif(rush_play == True):
-                        away_team['r_td'] += 1
-                    if(iterator < 42):
-                        away_team['q1_score'] += 6
-                    elif(iterator < 79):
-                        away_team['q2_score'] += 6
-                    elif(iterator < 118):
-                        away_team['q3_score'] += 6
-                    else:
-                        away_team['q4_score'] += 6
-                    xp = True
-                if(play == 'Field Goal'):
-                    if(xp == True):
-                        if(fg == 'TRUE'):
-                            away_team['q1_score'] += 1
-                            away_team['xpm'] += 1
-                        away_team['xpa'] += 1
-                        xp = False
-                    else:
-                        if(fg == 'TRUE'):
-                            away_team['q1_score'] += 3
-                            away_team['fgm'] += 1
-                        away_team['fga'] += 1
-
-                if(penalty == 'TRUE'):
-                    away_team['pen'] += 1
-                    if(was_accepted == 'TRUE'):
-                        away_team['pen_yds'] += penalty_yards
-                # Away Team
+                # Offense = Away Team; Defense = Home Team
+                play_allocation(away_team, home_team, row, iterator)
         iterator += 1
         # print(row)
 
